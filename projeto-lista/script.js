87065177956db86fa498bbcecd575b4c7ca801f2
dionatan03 <dionatan203@gmail.com>
2022-09-02 17:46:01 -0300

    update js

--- projeto-lista/script.js
+++ projeto-lista/script.js
@@ -1,33 +1,33 @@
-let minhaLista = document.getElementsByTagName("LI");
-for (let i = 0; i < minhaLista.length; i++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  minhaLista[i].appendChild(span);
-}
+const btnAdd = document.querySelector(".btn-adiciona");
 
-let close = document.getElementsByClassName("close");
-for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    let div = this.parentElement;
-    div.style.display = "none";
+function adicionaLI() {
+  let minhaLista = document.getElementsByTagName("LI");
+  for (let i = 0; i < minhaLista.length; i++) {
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    minhaLista[i].appendChild(span);
   }
 }
 
-let lista = document.querySelector('ul');
-lista.addEventListener('click', function(evento) {
-  if (evento.target.tagName === 'LI') {
-    evento.target.classList.toggle('checked');
+function btnFechar() {
+  let close = document.getElementsByClassName("close");
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.remove();
+      salvarDadosStorage();
+    };
   }
-}, false);
+}
 
 function criaLista() {
   let li = document.createElement("li");
   let inputValue = document.querySelector(".pega-valor").value;
   let t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue === "") {
     alert("você não digitou nada favor rever!");
   } else {
     document.querySelector(".criador-de-li").appendChild(li);
@@ -39,39 +39,54 @@ function criaLista() {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
-  for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      let div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  btnFechar();
 }
 
 function salvarDadosStorage() {
-  const liTarefas = document.querySelectorAll('LI');
+  const liTarefas = document.querySelectorAll("LI");
   const listaDeTarefas = [];
 
-  for (let tafefa of liTarefas) {
-    let tarefaTexto = tafefa.innerText;
-    tarefaTexto = tarefaTexto.replace('\u00D7', '').trim();
+  for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace("\u00D7", "").trim();
     listaDeTarefas.push(tarefaTexto);
   }
 
   const tarefasJson = JSON.stringify(listaDeTarefas);
-  console.log(listaDeTarefas);
+  localStorage.setItem("lista", tarefasJson);
 }
 
-const btnAdd = document.querySelector('.btn-adiciona');
+function adicionaListaSalvas() { //erro aquui na saida
+  const listas = localStorage.getItem("lista");
+  const recebeLista = JSON.parse(listas);
 
-btnAdd.addEventListener('click', () => {
-  criaLista();
-  salvarDadosStorage()
-});
-
-document.querySelector('.pega-valor').addEventListener('keypress', function (evento) {
-  if (evento.keyCode === 13) {
-    criaLista();
-    salvarDadosStorage()
+  for (let lista of recebeLista) {
+    console.log(lista);
   }
+}
+adicionaListaSalvas();
+
+let lista = document.querySelector("ul");
+lista.addEventListener(
+  "click",
+  function (evento) {
+    if (evento.target.tagName === "LI") {
+      evento.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+btnAdd.addEventListener("click", () => {
+  salvarDadosStorage();
+  criaLista();
 });
+
+document
+  .querySelector(".pega-valor")
+  .addEventListener("keypress", function (evento) {
+    if (evento.keyCode === 13) {
+      salvarDadosStorage();
+      criaLista();
+    }
+  });
